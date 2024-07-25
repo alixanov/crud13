76d9c3a31ffd6d2673b3ba289790ca87cfdd337d
Алиханов <alixonovshukurullo22@gmail.com>
2024-07-25 21:50:33 +0500

    start

--- src/components/Crud/Crud.jsx
+++ src/components/Crud/Crud.jsx
@@ -1,9 +1,7 @@
-
 import React, { useEffect, useState } from 'react'
 import "./crud.css"
 import axios from 'axios'
 import { Link, useNavigate } from 'react-router-dom'
-import "./crud.css"
 
 const Crud = () => {
   const [data, setData] = useState([])
@@ -13,13 +11,27 @@ const Crud = () => {
     axios
       .get("https://665ebce91e9017dc16f12c41.mockapi.io/groups")
       .then((res) => setData(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+  }, [])
 
-  },[])
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Malumotni uchirishni istaysizmi ?")
+
+    if (confirm) {
+      axios
+        .delete(`https://665ebce91e9017dc16f12c41.mockapi.io/groups/${id}`)
+        .then((res) => {
+          alert("Muvofiqiyatli uchirildi")
+          setData(data.filter((item) => item.id !== id)) // Удаление элемента из состояния
+        })
+        .catch((err) => console.log(err))
+    }
+  }
+
   return (
     <div className="table__container">
       <h2>Crud App with JSON Server</h2>
-      <Link to={"/create"}>Create</Link>
+      <Link to="/create">Create</Link>
       <table>
         <thead>
           <tr>
@@ -27,7 +39,7 @@ const Crud = () => {
             <th>Familya</th>
             <th>Telefon raqami</th>
             <th>Email</th>
-            <th>ACtion</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -40,34 +52,20 @@ const Crud = () => {
                 <td>{item.email}</td>
                 <td>
                   <Link to={`update/${item.id}`}>Update</Link>
-                  <button onClick={(e) => handleDelete(item.id)}>Delete</button>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
                   <Link to={`reading/${item.id}`}>Reading</Link>
                 </td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="4">Malumot yuklanmadi</td>
+              <td colSpan="5">Malumot yuklanmadi</td>
             </tr>
           )}
         </tbody>
       </table>
     </div>
-  );
-
-
-  function handleDelete(id) {
-const confirm = window.confirm("Malumotni uchirishni istaysizmi ?")
-
-    if(confirm){
-       axios
-         .delete(`https://668f62c780b313ba0917e986.mockapi.io/register/${id}`  )
-         .then((res) => {
-           alert("Muvofiqiyatli uchirildi");
-           navigate("/");
-         });
-   }
-  }
+  )
 }
 
 export default Crud
